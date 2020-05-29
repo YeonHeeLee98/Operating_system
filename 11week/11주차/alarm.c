3bdc8706164ba1843ab472090f69ce40e84741e8
YeonHeeLee98 <YHLee98y@gmail.com>
2020-05-29 13:58:48 +0900

    Upload Study file

---
+++ 11week/11주차/alarm.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+
+void alarmHandler(int signo);
+
+int main(int args, char **argv){
+	int status;
+	struct sigaction act;
+
+	act.sa_handler=alarmHandler;
+	sigemptyset(&act.sa_mask);
+	act.sa_flags=0;
+
+	sigaction(SIGALRM,&act,NULL);
+
+	alarm(3);
+	while(1)
+		;
+}
+
+void alarmHandler(int signo){
+	printf("Hi signal %d\n",signo);
+	exit(0);
+}
